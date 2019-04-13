fe6028a0a257b89fa7b8ba28be3e3363f7cace99
umutdr <umutdr98@gmail.com>
2019-04-14 00:13:46 +0300

    Add project files.

---
+++ Screenshot-of-the-form/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Screenshot_of_the_form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(379, 116);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(216, 150);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(158, 268);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(80, 17);
+            this.checkBox2.TabIndex = 11;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(541, 11);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(645, 192);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(80, 17);
+            this.checkBox3.TabIndex = 13;
+            this.checkBox3.Text = "checkBox3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(855, 339);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox3;
+    }
+}
+
---
+++ Screenshot-of-the-form/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace Screenshot_of_the_form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static void SaveScreenshot(Form frm)
+        {
+            int recStartX = 8;  // Sol border width 
+            int recStartY = 31; // Üstteki Pencere çubuğunun yüksekliği 
+                                // recStartX değerinin iki katını almamın sebebi, hem solda hemde sağ tarafta 8px border olduğu için toplam 8px*2 olmasıdır.
+                                // recStartX değeri ile recStartY değerinin toplanmasının sebebi, üstte pencere çubuğu 31px ve alttaki border 8px olduğundan 31px+8px olmasıdır.
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Filter = "PNG | *.png|JPG | *.jpg|JPEG | *.jpeg|All files (*.*)|*.*";
+            saveFile.OverwritePrompt = true;
+            saveFile.FileName = string.Format(@"Thornthwaite-Grafik_{0}",DateTime.Now.Ticks);
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                string WhereToSaveFile = saveFile.FileName;
+                Bitmap Image = new Bitmap(frm.Width, frm.Height); // Form boyutunda bir tuval oluşturuluyor
+                frm.DrawToBitmap(Image, new Rectangle(0,0,frm.Width,frm.Height)); // oluşturulan tuvale form penceresinin görüntüsü çiziliyor
+                Image = Image.Clone(new Rectangle(recStartX, recStartY, Image.Size.Width - (recStartX * 2), Image.Size.Height - (recStartX + recStartY)), PixelFormat.DontCare);
+                // Image değişkeninde tutulan form görüntüsü üzerinde kırpma işlemi gerçekleştiriliyor ve Image değişkenine aktarılıyor.
+                Image.Save(WhereToSaveFile, ImageFormat.Png);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveScreenshot(ActiveForm);
+        }
+    }
+}
